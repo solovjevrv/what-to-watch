76377f4f511574c8aa32cdb0a473465afb6d2407
solovjevrv <solovjevrv@gmail.com>
2023-06-10 15:05:13 +0300

    basic project structure

---
+++ wtw/src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div><h1>Header</h1></div>
+</template>
+
+<script>
+  export default {
+    name: "HeaderComponent",
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ wtw/src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><HeaderComponent /></div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import HeaderComponent from "@/components/HeaderComponent";
+
+  export default {
+    name: "HomePage",
+    components: {
+      HeaderComponent,
+    },
+  };
+</script>
