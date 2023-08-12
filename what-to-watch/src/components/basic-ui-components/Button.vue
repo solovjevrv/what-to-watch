3fb9381cd6d89b14605fe34dc6974328e6e125b2
solovjevrv <solovjevrv@gmail.com>
2023-08-12 20:55:27 +0300

    basic structure + button component

---
+++ what-to-watch/src/components/basic-ui-components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    class="c-button"
+    :class="[
+      kind ? 'c-button--' + kind : '',
+      size ? 'c-button--size-' + size : '',
+      {'c-button--is-disabled': disabled},
+    ]"
+    :disabled="disabled"
+    :type="nativeType"
+    @click="handleClick"
+  >
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'c-button',
+  props: {
+    nativeType: {
+      type: String,
+      default: 'button',
+    },
+    kind: {
+      type: String,
+      default: 'primary',
+    },
+    size: {
+      type: String,
+      default: 'lg',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleClick(event) {
+      this.$emit('click', event);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ what-to-watch/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Something Title</h1>
+    <c-button kind="primary" @click="func">Button</c-button>
+    <c-button kind="secondary" @click="func">Button</c-button>
+    <c-button kind="tertiary" @click="func">Button</c-button>
+    <c-button kind="ghost" @click="func">Button</c-button>
+    <c-button kind="danger" @click="func">Button</c-button>
+    <c-button kind="danger-tertiary" @click="func">Button</c-button>
+    <c-button kind="danger-ghost" @click="func">Button</c-button>
+  </div>
+</template>
+
+<script>
+import cButton from '@/components/basic-ui-components/Button.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    cButton,
+  },
+  methods: {
+    func() {
+      console.log('Event!');
+    },
+  },
+};
+</script>
